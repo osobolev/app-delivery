4c840dd227dae5c7be8456c7416e331dfe4d3d9d
Oleg Sobolev <sobolev_ov@mail.ru>
2020-01-19 03:34:31 +0300

    Move to new sqlg2

--- doc/example/sample_server/src/sample/SampleInit.java
+++ doc/example/sample_server/src/sample/SampleInit.java
@@ -2,13 +2,21 @@ package sample;
 
 import server.core.AppInit;
 import server.core.LoginData;
+import sqlg2.db.IServerSerializer;
 import sqlg2.db.SQLGLogger;
+import sqlg2.db.ServerJavaSerializer;
 import sqlg2.db.SessionFactory;
 
 public final class SampleInit implements AppInit {
 
+    private final ServerJavaSerializer serializer = new ServerJavaSerializer();
+
     public SQLGLogger createLogger() {
         return new SQLGLogger.Simple();
+    }
+
+    public IServerSerializer getSerializer() {
+        return serializer;
     }
 
     public SessionFactory init(String application, LoginData data) {
