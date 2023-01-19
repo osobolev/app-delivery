e02c175b337d87592e2cf05422470204a2a49c06
Oleg Sobolev <sobolev_ov@mail.ru>
2023-01-19 16:23:48 +0100

    Рефакторинг

--- server/install/src/server/install/Profile.java
+++ server/install/src/server/install/Profile.java
@@ -8,8 +8,6 @@ import java.util.Properties;
 
 final class Profile {
 
-    private static final Profile NONE = new Profile(null);
-
     private final String name;
 
     private Profile(String name) {
@@ -24,21 +22,21 @@ final class Profile {
         }
     }
 
-    private String getPropFileName(String baseName) {
-        return name == null ? baseName : baseName + "_" + name;
+    private static String getPropFileName(String profileName, String baseName) {
+        return profileName == null ? baseName : baseName + "_" + profileName;
     }
 
-    private File getPropFile(File root, String baseName) {
-        String fileName = getPropFileName(baseName) + ConfigReader.PROPERTIES;
+    private static File getPropFile(File root, String profileName, String baseName) {
+        String fileName = getPropFileName(profileName, baseName) + ConfigReader.PROPERTIES;
         File file = new File(root, fileName);
         if (file.exists())
             return file;
         return null;
     }
 
-    private Properties loadProps(File root) {
+    private static Properties loadProps(File root, String profileName) {
         Properties props = new Properties();
-        File propFile = getPropFile(root, "install");
+        File propFile = getPropFile(root, profileName, "install");
         if (propFile != null) {
             ConfigReader.readProperties(props, propFile);
         }
@@ -47,13 +45,13 @@ final class Profile {
 
     Properties loadProfileProps(File root) {
         // Loading install.properties:
-        Properties installProps = NONE.loadProps(root);
+        Properties installProps = loadProps(root, null);
         if (name != null) {
             // Override install.properties only if there is a  profile
             Properties profileProps = new Properties();
             profileProps.putAll(installProps);
             // Loading install_<profile>.properties:
-            Properties props = loadProps(root);
+            Properties props = loadProps(root, name);
             for (String name : props.stringPropertyNames()) {
                 String value = props.getProperty(name);
                 if (value == null || value.trim().isEmpty()) {
@@ -85,12 +83,12 @@ final class Profile {
     }
 
     File findPropFile(File root, String baseName) {
-        File myFile = getPropFile(root, baseName);
+        File myFile = getPropFile(root, name, baseName);
         if (myFile != null)
             return myFile;
         if (name != null) {
             // Try to find file without "_<profile>" suffix:
-            return NONE.getPropFile(root, baseName);
+            return getPropFile(root, null, baseName);
         } else {
             return null;
         }
