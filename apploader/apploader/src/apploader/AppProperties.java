a4da90396f29e4544006a229af821533c46d8028
Oleg Sobolev <sobolev_ov@mail.ru>
2023-01-24 09:12:06 +0100

    Рефакторинг запуска tzupdater

--- apploader/apploader/src/apploader/AppProperties.java
+++ apploader/apploader/src/apploader/AppProperties.java
@@ -5,7 +5,6 @@ import apploader.common.ConfigReader;
 import apploader.lib.FileResult;
 import apploader.lib.IFileLoader;
 import apploader.lib.ILoaderGui;
-import apploader.lib.Result;
 
 import java.io.File;
 import java.io.IOException;
@@ -40,8 +39,7 @@ final class AppProperties {
             return null;
         fileLoader.receiveFile(AppCommon.getSplashName(application), true, true);
         AppProperties properties = new AppProperties();
-        FileResult[] newTZUpdater = new FileResult[1];
-        FileResult[] newTimeZones = new FileResult[1];
+        TZUpdater tzUpdater = new TZUpdater();
         JavaUpdater javaUpdater = new JavaUpdater();
         boolean ok = ConfigReader.readConfig(list, (left, right) -> {
             boolean jar;
@@ -93,11 +91,7 @@ final class AppProperties {
                 File file = fileResult.file;
                 if (file == null)
                     return false;
-                if ("tzupdater.jar".equals(right)) {
-                    newTZUpdater[0] = fileResult;
-                } else if ("tzdata.tar.gz".equals(right)) {
-                    newTimeZones[0] = fileResult;
-                }
+                tzUpdater.add(right, fileResult);
             } else if ("?file".equalsIgnoreCase(left)) {
                 fileLoader.receiveFile(right, true, true);
             } else if ("mainClass".equalsIgnoreCase(left)) {
@@ -111,57 +105,8 @@ final class AppProperties {
             return null;
         if (!javaUpdater.update(gui, fileLoader))
             return null;
-        if (newTZUpdater[0] != null && newTimeZones[0] != null) {
-            boolean anyUpdated = newTZUpdater[0].updated || newTimeZones[0].updated;
-            Boolean updateResult = updateTimeZones(gui, fileLoader, anyUpdated, newTimeZones[0].file);
-            if (updateResult == null)
-                return null;
-            if (updateResult.booleanValue()) {
-                gui.showWarning("Обновлены данные часовых поясов, перезапустите приложение");
-                return null;
-            }
-        }
+        if (!tzUpdater.update(gui, fileLoader))
+            return null;
         return properties;
-    }
-
-    private static Boolean updateTimeZones(ILoaderGui gui, IFileLoader fileLoader, boolean newFiles, File dataFile) {
-        File successFile = fileLoader.getLocalFile("tzupdater.done");
-        if (successFile.exists() && !newFiles)
-            return false;
-        File javaHome = JavaUpdater.getLocalJRE(fileLoader);
-        if (javaHome == null) {
-            // Our java is not in "jre" folder, cannot update it
-            return false;
-        }
-        boolean success = false;
-        try {
-            File javaBin = new File(new File(javaHome, "bin"), AppCommon.isWindows() ? "java.exe" : "java");
-            ProcessBuilder pb = new ProcessBuilder(
-                javaBin.getAbsolutePath(), "-jar", "tzupdater.jar", "-v", "-f", "-l", dataFile.toURI().toString()
-            );
-            pb.redirectErrorStream(true);
-            pb.redirectOutput(ProcessBuilder.Redirect.appendTo(fileLoader.getLocalFile("tzupdater.log")));
-            Process process = pb.start();
-            int exitCode = process.waitFor();
-            boolean ok;
-            if (exitCode == 0) {
-                ok = true;
-            } else {
-                Result ans = gui.showWarning3("Ошибка обновления часовых поясов", null);
-                if (ans == Result.ABORT)
-                    return null;
-                ok = ans == Result.IGNORE;
-            }
-            if (ok) {
-                successFile.createNewFile();
-            }
-            success = ok;
-        } catch (Exception ex) {
-            AppCommon.error(ex);
-        }
-        if (!success) {
-            successFile.delete();
-        }
-        return true;
     }
 }
---
+++ apploader/apploader/src/apploader/TZUpdater.java
@@ -0,0 +1,84 @@
+package apploader;
+
+import apploader.common.AppCommon;
+import apploader.lib.FileResult;
+import apploader.lib.IFileLoader;
+import apploader.lib.ILoaderGui;
+import apploader.lib.Result;
+
+import java.io.File;
+
+final class TZUpdater {
+
+    private FileResult newTZUpdater = null;
+    private FileResult newTimeZones = null;
+
+    void add(String right, FileResult fileResult) {
+        if ("tzupdater.jar".equals(right)) {
+            newTZUpdater = fileResult;
+        } else if ("tzdata.tar.gz".equals(right)) {
+            newTimeZones = fileResult;
+        }
+    }
+
+    boolean update(ILoaderGui gui, IFileLoader fileLoader) {
+        if (newTZUpdater != null && newTimeZones != null) {
+            boolean anyUpdated = newTZUpdater.updated || newTimeZones.updated;
+            Boolean updateResult = updateTimeZones(gui, fileLoader, anyUpdated, newTimeZones.file);
+            if (updateResult == null)
+                return false;
+            if (updateResult.booleanValue()) {
+                gui.showWarning("Обновлены данные часовых поясов, перезапустите приложение");
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * @return
+     *   false, если не нужно обновлять TZDB;
+     *   true, если TZDB обновлена и нужен перезапуск;
+     *   null, если ошибка при обновлении.
+     */
+    private static Boolean updateTimeZones(ILoaderGui gui, IFileLoader fileLoader, boolean newFiles, File dataFile) {
+        File successFile = fileLoader.getLocalFile("tzupdater.done");
+        if (successFile.exists() && !newFiles)
+            return false;
+        File javaHome = JavaUpdater.getLocalJRE(fileLoader);
+        if (javaHome == null) {
+            // Our java is not in "jre" folder, cannot update it
+            return false;
+        }
+        boolean success = false;
+        try {
+            File javaBin = new File(new File(javaHome, "bin"), AppCommon.isWindows() ? "java.exe" : "java");
+            ProcessBuilder pb = new ProcessBuilder(
+                javaBin.getAbsolutePath(), "-jar", "tzupdater.jar", "-v", "-f", "-l", dataFile.toURI().toString()
+            );
+            pb.redirectErrorStream(true);
+            pb.redirectOutput(ProcessBuilder.Redirect.appendTo(fileLoader.getLocalFile("tzupdater.log")));
+            Process process = pb.start();
+            int exitCode = process.waitFor();
+            boolean ok;
+            if (exitCode == 0) {
+                ok = true;
+            } else {
+                Result ans = gui.showWarning3("Ошибка обновления часовых поясов", null);
+                if (ans == Result.ABORT)
+                    return null;
+                ok = ans == Result.IGNORE;
+            }
+            if (ok) {
+                successFile.createNewFile();
+            }
+            success = ok;
+        } catch (Exception ex) {
+            AppCommon.error(ex);
+        }
+        if (!success) {
+            successFile.delete();
+        }
+        return true;
+    }
+}
