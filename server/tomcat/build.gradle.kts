aac224494137b73bebcc446e3969c46ce749cc34
Oleg Sobolev <sobolev_ov@mail.ru>
2023-06-27 13:25:45 +0200

    Обновление зависимостей

--- server/tomcat/build.gradle.kts
+++ server/tomcat/build.gradle.kts
@@ -3,6 +3,6 @@ plugins {
 }
 
 dependencies {
-    implementation("org.apache.tomcat.embed:tomcat-embed-core:9.0.72")
+    implementation("org.apache.tomcat.embed:tomcat-embed-core:9.0.76")
     api(project(":server-embedded"))
 }
