0e49a596dc870a8c74435cb6d16d70cd5d05cc86
Oleg Sobolev <sobolev_ov@mail.ru>
2023-11-24 05:59:53 +0100

    Обновление зависимостей

--- server/install/build.gradle.kts
+++ server/install/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 dependencies {
-    implementation("org.apache.commons:commons-compress:1.24.0")
+    implementation("org.apache.commons:commons-compress:1.25.0")
     implementation("org.tukaani:xz:1.9")
     implementation(project(":apploader-common"))
 }
--- server/tomcat/build.gradle.kts
+++ server/tomcat/build.gradle.kts
@@ -3,6 +3,6 @@ plugins {
 }
 
 dependencies {
-    implementation("org.apache.tomcat.embed:tomcat-embed-core:9.0.81")
+    implementation("org.apache.tomcat.embed:tomcat-embed-core:9.0.83")
     api(project(":server-embedded"))
 }
