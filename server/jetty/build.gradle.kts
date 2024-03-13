42855293d5bb5263d7999b4abcf3218435870b0b
Oleg Sobolev <sobolev_ov@mail.ru>
2024-03-13 11:00:16 +0100

    Обновление зависимостей

--- server/jetty/build.gradle.kts
+++ server/jetty/build.gradle.kts
@@ -3,6 +3,6 @@ plugins {
 }
 
 dependencies {
-    implementation("org.eclipse.jetty:jetty-servlet:9.4.53.v20231009")
+    implementation("org.eclipse.jetty:jetty-servlet:9.4.54.v20240208")
     api(project(":server-embedded"))
 }
