0455760b53653fc319e40621e848b789f134d2ed
Oleg Sobolev <sobolev_ov@mail.ru>
2024-06-19 15:31:56 +0200

    Версия 6.8

--- buildSrc/src/main/kotlin/lib.gradle.kts
+++ buildSrc/src/main/kotlin/lib.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "io.github.osobolev.app-delivery"
-version = "6.7"
+version = "6.8"
 
 (publishing.publications["mavenJava"] as MavenPublication).pom {
     name.set("app-delivery")
