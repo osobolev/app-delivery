d9fc5c7d184e2b91c8fd127851710317590f208d
Oleg Sobolev <sobolev_ov@mail.ru>
2024-10-08 10:28:31 +0200

    Изменения unix-unzip

--- apploader/common/src/apploader/common/AppZip.java
+++ apploader/common/src/apploader/common/AppZip.java
@@ -1,48 +1,38 @@
 package apploader.common;
 
-import io.github.osobolev.unixzip.UnixZipExtra;
+import io.github.osobolev.unixunzip.UnixUnzip;
+import io.github.osobolev.unixunzip.UnixZipExtra;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Collections;
-import java.util.Enumeration;
 import java.util.Map;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
 
 public final class AppZip {
 
-    private final File file;
+    private final Path file;
     private final Map<String, UnixZipExtra> extras;
 
-    private AppZip(File file, Map<String, UnixZipExtra> extras) {
+    private AppZip(Path file, Map<String, UnixZipExtra> extras) {
         this.file = file;
         this.extras = extras;
     }
 
     public static AppZip create(File file) {
+        Path path = file.toPath();
         Map<String, UnixZipExtra> extras = Collections.emptyMap();
         if (!AppCommon.isWindows()) {
             try {
-                extras = UnixZipExtra.readExtras(file);
+                extras = UnixZipExtra.readExtras(path);
             } catch (IOException ex) {
                 // ignore
             }
         }
-        return new AppZip(file, extras);
+        return new AppZip(path, extras);
     }
 
     public void unpackWithExtra(Path destDir, Runnable onEntry) throws IOException {
-        try (ZipFile zipFile = new ZipFile(file)) {
-            Enumeration<? extends ZipEntry> entries = zipFile.entries();
-            while (entries.hasMoreElements()) {
-                ZipEntry entry = entries.nextElement();
-                if (onEntry != null) {
-                    onEntry.run();
-                }
-                UnixZipExtra.restoreEntry(zipFile, extras, entry, destDir);
-            }
-        }
+        UnixUnzip.unzip(file, destDir, extras, entry -> onEntry.run());
     }
 }
