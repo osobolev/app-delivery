91dcea939f29ff2c217719a74b5df67e8aa4de5e
Oleg Sobolev <sobolev_ov@mail.ru>
2025-07-10 07:07:41 +0200

    Версия 8.5

--- buildSrc/src/main/kotlin/lib.gradle.kts
+++ buildSrc/src/main/kotlin/lib.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "io.github.osobolev.app-delivery"
-version = "8.4"
+version = "8.5"
 
 if (project.name == "unix-unzip") {
     description = "Library for reading/restoring UNIX permissions of ZIP file entries";
