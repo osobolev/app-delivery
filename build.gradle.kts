c83649bd3af96c0383266a8806e85158c6c8723d
Oleg Sobolev <sobolev_ov@mail.ru>
2026-07-29 11:47:19 +0200

    Обновление зависимостей

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,3 @@
-plugins {
-    id("com.github.ben-manes.versions") version "0.54.0"
-}
-
 fun requiredMajor(mod: ModuleComponentIdentifier): String {
     if (mod.group == "javax.servlet") return "3." // версия >= 4 использует jakarta namespace
     if (mod.group == "org.apache.tomcat.embed") return "9." // версия >= 10 использует jakarta namespace
